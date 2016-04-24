b24379de90e4af31b34ad170e01135979d6af3ff
Leonid Bushuev <leonidos@gmail.com>
2016-04-24 21:34:20 +0200

    Init Idea environment

---
+++ plugin/src/org/jetbrains/dekaf/idea/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Dekaf Supporting Plugin for IntelliJ Idea.
+ *
+ * <p>
+ *   Dekaf: <a href="http://github.com/JetBrains/dekaf">Dekaf on GitHub</a>
+ * </p>
+ *
+ * @author Leonid Bushuev
+ **/
+package org.jetbrains.dekaf.idea;
